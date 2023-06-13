534be8a51872da5f5cbaa7fa8f44c6c53f491681
Jikun <jikunchen@microsoft.com>
2023-06-13 16:55:47 +0800

    edit api

--- api/message/index.js
+++ api/message/index.js
@@ -7,16 +7,16 @@
 module.exports = async function (context, req) {
     context.log("JavaScript HTTP trigger function processed a request.");
   
-    const authorization = req.headers["Authorization"];
+    const authorization = req.headers["authorization"];
     if (authorization && authorization === "BearerSecret") {
       context.res = {
         body: { text: "Hello from the API" },
       };
     }
     else {
-      context.res = {
-        status: 401,
-        body: "Unauthorized",
-      };
-    }
+        context.res = {
+          status: 401,
+          body: "Unauthorized",
+        };
+      }
   };
