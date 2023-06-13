207a39ad4706f42a0f68cc83ed737e4d95e61bf3
Jikun <jikunchen@microsoft.com>
2023-06-13 16:29:54 +0800

    remove unused api

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,22 @@
+// module.exports = async function (context, req) {
+//     context.res.json({
+//         text: "Hello from the API"
+//     });
+// };
+
 module.exports = async function (context, req) {
-    context.res.json({
-        text: "Hello from the API"
-    });
-};
+    context.log("JavaScript HTTP trigger function processed a request.");
+  
+    const authorization = req.headers["Authorization"];
+    if (authorization && authorization === "BearerSecret") {
+      context.res = {
+        body: { text: "Hello from the API" },
+      };
+    }
+    else {
+      context.res = {
+        status: 401,
+        body: "Unauthorized",
+      };
+    }
+  };
