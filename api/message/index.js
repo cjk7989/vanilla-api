076483a2e22e1b7a7321ca9925dcdd87bd62f114
Jikun <jikunchen@microsoft.com>
2023-09-11 11:39:47 +0800

    .

--- api/message/index.js
+++ api/message/index.js
@@ -6,21 +6,7 @@
 
 module.exports = async function (context, req) {
     context.log("JavaScript HTTP trigger function processed a request.");
-  
-    const authorization = req.headers["x-cjk-token"];
-    if (authorization && authorization === "BearerSecret") {
-        context.res = {
-        // body: { text: "Hello from the API" },
-        body: { text: "Hello from the API + " + JSON.stringify(req.headers) },
-        };
-    }
-    else {
-        // context.res = {
-        //   status: 401,
-        //   body: "Unauthorized",
-        // };
-        context.res = {
-            body: { text: "Fail + " + JSON.stringify(req.headers) },
-        };
-    }
+	context.res = {
+		body: { text: "Hello from the API" },
+	};
 };
