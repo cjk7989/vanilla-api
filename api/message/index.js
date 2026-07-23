8e14ac4400c5854bc69123c7b81d382890245ca7
Jikun <36817715+cjk7989@users.noreply.github.com>
2026-07-23 19:51:45 +0800

    Update index.js

--- api/message/index.js
+++ api/message/index.js
@@ -7,6 +7,6 @@
 module.exports = async function (context, req) {
     context.log("JavaScript HTTP trigger function processed a request.");
 	context.res = {
-		body: { text: "Hello from the API" },
+		body: { text: "Hello from the API v2" },
 	};
 };
